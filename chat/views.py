35231b6ba2050ad5f5a9b5a9a84a8c5415741136
0lipa <brain_2015@naver.com>
2025-10-24 16:13:59 +0900

    디자인 수정하고 로그아웃 연결했습니다

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib import messages
+
+def logout_view(request):
+    logout(request)  # 현재 로그인된 사용자 세션을 종료
+    messages.success(request, '로그아웃 되었습니다.')
+    return redirect('uauth:main')  # 로그아웃 후 이동할 페이지
 
 def withdraw_view(request):
     return render(request, 'chat/withdraw.html')
 
 def chat_main(request):
     return render(request, 'chat/chat.html')
-
-def logout(request):
-    return render(request, 'chat/logout.html')
