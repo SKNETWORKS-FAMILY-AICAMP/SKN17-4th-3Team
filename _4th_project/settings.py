269e11b2d3d4dd33d402f362cd8ff360a2a8e07a
0lipa <brain_2015@naver.com>
2025-10-27 12:48:14 +0900

    Update settings.py

--- _4th_project/settings.py
+++ _4th_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', "127.0.0.1",'15.165.171.5','3.37.124.207']
 
